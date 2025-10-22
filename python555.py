34a201a130422032b7e4d1b8928e841ec7b5df60
arielolin <114854641+arielolin@users.noreply.github.com>
2025-10-22 16:04:07 +0300

    Remove duplicate entries in python555.py
    Removed duplicate lines related to ariel-repo and password.

--- python555.py
+++ python555.py
@@ -15,3 +15,20 @@ password="123456"
 the ariel org main repo
 password="123456"
 password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
