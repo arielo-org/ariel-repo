fc91272f5f522ed194cbb331c44be2c7bf15a88e
arielolin <114854641+arielolin@users.noreply.github.com>
2025-10-23 08:54:15 +0300

    Clean up duplicate repository entries (#42)
    Removed duplicate entries of repository information and password.

--- python555.py
+++ python555.py
@@ -34,3 +34,40 @@ password="123456"
 the ariel org main repo
 password="123456"
 password="123456"
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
