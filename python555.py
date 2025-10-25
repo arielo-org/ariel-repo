159fc1cdd7e25b5391b503014386012e39f8558e
arielolin <114854641+arielolin@users.noreply.github.com>
2025-10-25 20:58:18 +0300

    Remove duplicate entries in python555.py

--- python555.py
+++ python555.py
@@ -16,3 +16,22 @@ the ariel org main repo
 password="123456"
 password="123456"
 
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+# ariel-repo
+the ariel org main repo
+password="123456"
+password="123456"
+
