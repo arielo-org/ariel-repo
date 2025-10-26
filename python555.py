8fb41bf2a98a4263e3ab8520ce61376b4285980d
arielolin <114854641+arielolin@users.noreply.github.com>
2025-10-26 12:04:47 +0200

    Update python555.py (#56)

--- python555.py
+++ python555.py
@@ -3,16 +3,4 @@ the ariel org main repo
 password="123456"
 password="123456"
 
-# ariel-repo
-the ariel org main repo
-password="123456"
-password="123456"
-# ariel-repo
-the ariel org main repo
-password="123456"
-password="123456"
-# ariel-repo
-the ariel org main repo
-password="123456"
-password="123456"
 
