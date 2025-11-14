6e4e26588d2da051c9ffb69f2f27257478a1703e
arielolin <114854641+arielolin@users.noreply.github.com>
2025-11-14 09:53:33 +0200

    Add password4 and additional URL to secrets.py
    Added new password variable and URL.

--- secrets.py
+++ secrets.py
@@ -8,3 +8,6 @@ https://github.com/arielo-org/ariel-repo
 password2="1232444434234"
 https://github.com/arielo-org/ariel-repo
 
+password4="1232444rrrazd434234"
+https://github.com/arielo-org/ariel-repo
+
