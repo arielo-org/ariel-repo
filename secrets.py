f5c05d412844ff722692d3bf912c72f93137574c
arielolin <114854641+arielolin@users.noreply.github.com>
2025-11-14 09:54:39 +0200

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -11,3 +11,6 @@ https://github.com/arielo-org/ariel-repo
 password4="1232444rrrazd434234"
 https://github.com/arielo-org/ariel-repo
 
+password5="1232444r455rrazd434234"
+https://github.com/arielo-org/ariel-repo
+
