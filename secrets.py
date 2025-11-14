64bbe7eeca54c62efede6ca4c5002a0e9cf4572e
arielolin <114854641+arielolin@users.noreply.github.com>
2025-11-14 10:04:20 +0200

    Add new passwords to secrets.py

--- secrets.py
+++ secrets.py
@@ -11,3 +11,6 @@ https://github.com/arielo-org/ariel-repo
 password5="1232444rrrazd434234"
 https://github.com/arielo-org/ariel-repo
 
+password45="1232444rr44razd434234"
+https://github.com/arielo-org/ariel-repo
+
