a0fa17991951c5eab7da446a3ec8e5eb4aa6b618
arielolin <114854641+arielolin@users.noreply.github.com>
2025-11-14 09:54:58 +0200

    Add password5 variable to secrets.py

--- secrets.py
+++ secrets.py
@@ -8,6 +8,6 @@ https://github.com/arielo-org/ariel-repo
 password2="1232444434234"
 https://github.com/arielo-org/ariel-repo
 
-password4="1232444rrrazd434234"
+password5="1232444rrrazd434234"
 https://github.com/arielo-org/ariel-repo
 
